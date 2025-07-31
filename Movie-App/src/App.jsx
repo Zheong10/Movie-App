51745c9ac04e3b7acdb01837769c4c844f850e5f
Zheong10 <zuhar.20232323@iit.ac.lk>
2025-08-01 05:14:15 +0530

    Updated App.jsx

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -20,10 +20,9 @@ const API_OPTIONS = {
 const App = () => {
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-
   const [movieList, setMovieList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [trendingMovies, setTrendingMovies] = useState([]);
 
@@ -56,6 +55,8 @@ const App = () => {
 
       setMovieList(data.results || []);
 
+      updateSearchCount();
+
       if (query && data.results.length > 0) {
         await updateSearchCount(query, data.results[0]);
       }
@@ -63,7 +64,7 @@ const App = () => {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
-      setIsLoading(true);
+      setIsLoading(false);
     }
   };
 
@@ -99,13 +100,14 @@ const App = () => {
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[30px]">All Movies</h2>
 
           {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
-            <p className="text-red-500">{errorMessage}</p>
+            <p className="text-red-500 ">{errorMessage}</p>
           ) : (
             <ul>
               {movieList.map((movie) => (
